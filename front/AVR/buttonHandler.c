e603c6ed188f9a78e3eea20d018d29f03c8ee9ad
Julian Schmidt <julian@sonic-potions.com>
2013-06-01 12:49:33 +0200

    fixed double triggering (toggle) of select buttons in voice mode

--- front/AVR/buttonHandler.c
+++ front/AVR/buttonHandler.c
@@ -662,6 +662,7 @@ void buttonHandler_buttonReleased(uint8_t buttonNr)
 			}
 			else
 			{
+				if(buttonHandler_stateMemory.selectButtonMode == SELECT_MODE_VOICE) return;
 				//do nothing if timer action occured
 				if(buttonHandler_TimerActionOccured())return;
 				
