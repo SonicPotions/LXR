7df9aeeca20c868023f396327d5df6fd55ad3135
Julian Schmidt <julian@sonic-potions.com>
2013-07-21 09:26:33 +0200

    Delete makeFirmware.bat

--- tools/FirmwareImageBuilder/Debug/makeFirmware.bat
+++
@@ -1,4 +0,0 @@
-copy E:\gewerbe_sonic_potions\git\Drumsynth\front\AVR\Release\DrumSynthAvr.bin E:\gewerbe_sonic_potions\git\Drumsynth\tools\FirmwareImageBuilder\Debug\DrumSynthAvr.bin 
-copy E:\gewerbe_sonic_potions\git\Drumsynth\mainboard\firmware\DrumSynth_FPU\Debug\DrumSynthCortex.bin E:\gewerbe_sonic_potions\git\Drumsynth\tools\FirmwareImageBuilder\Debug\DrumSynthCortex.bin 
-
-call FirmwareImageBuilder.exe
