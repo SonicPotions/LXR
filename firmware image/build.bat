1aac6797ca606a2c48d8d70e0d9500d3062e21e7
Andrew Shakinovsky <andrew@binarydog.com>
2013-11-23 15:19:42 -0500

    rem dup files

--- firmware image/build.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-del old_FIRMWARE.BIN 2>nul
-ren FIRMWARE.BIN old_FIRMWARE.BIN 2>nul
-firmwareimagebuilder ..\mainboard\firmware\DrumSynth_FPU\Release\DrumSynth_FPU.bin ..\front\AVR\Release\DrumSynthAvr.bin FIRMWARE.BIN
