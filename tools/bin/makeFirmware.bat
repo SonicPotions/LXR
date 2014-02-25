71ebd327fd649d08eb61d0ce382f807fba3c1db4
Julian Schmidt <julian@sonic-potions.com>
2014-02-25 15:37:36 +0100

    commit modified make.bat due to branch merge

--- tools/bin/makeFirmware.bat
+++ tools/bin/makeFirmware.bat
@@ -1,4 +1,6 @@
 del "..\..\firmware image\old_FIRMWARE.BIN" 2>nul
 ren "..\..\firmware image\FIRMWARE.BIN" old_FIRMWARE.BIN 2>nul
-firmwareimagebuilder ..\..\mainboard\firmware\DrumSynth_FPU\Release\DrumSynth_FPU.bin ..\..\front\AVR\Release\DrumSynthAvr.bin "..\..\firmware image\FIRMWARE.BIN"
-pause
+
+call avr-objcopy -O binary -R .eeprom -R .fuse -R .lock -R .signature  "..\..\front\AVR\Release\AVR.elf" "DrumSynthAvr.bin"
+
+firmwareimagebuilder ..\..\mainboard\firmware\DrumSynth_FPU\Debug\DrumSynthCortex.bin DrumSynthAvr.bin "..\..\firmware image\FIRMWARE.BIN"
