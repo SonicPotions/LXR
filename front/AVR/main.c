653b2a7139c3d170edb87bfb75c6594179a762e6
Julian Schmidt <julian@sonic-potions.com>
2014-05-05 16:19:19 +0200

    updated version number to 0.33

--- front/AVR/main.c
+++ front/AVR/main.c
@@ -28,7 +28,7 @@
 //-----------------------------------------------
 //defines
 //-----------------------------------------------
-#define FIRMWARE_VERSION "0.32"
+#define FIRMWARE_VERSION "0.33"
 #define CORTEX_RESET_PIN	PB0
 //-----------------------------------------------
 //code
@@ -75,7 +75,7 @@ int main(void)
 	lcd_string_F(PSTR("Sonic Potions"));
 	//goto 2nd line
 	lcd_setcursor(0,2);
-	lcd_string_F(PSTR("LXR*Drums V"));
+	lcd_string_F(PSTR("LXR Drums V"));
 	lcd_string(FIRMWARE_VERSION);
 #endif
 
