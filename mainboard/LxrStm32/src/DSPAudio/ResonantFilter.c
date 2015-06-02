3a970715f654ab7f6774b06a4abd4b9b1832a9dc
Julian Schmidt <julian@sonic-potions.com>
2015-06-02 17:26:22 +0200

    improved LP2 resonance scaling

--- mainboard/LxrStm32/src/DSPAudio/ResonantFilter.c
+++ mainboard/LxrStm32/src/DSPAudio/ResonantFilter.c
@@ -161,7 +161,8 @@ void SVF_calcBlockZDF(ResonantFilter* filter, const uint8_t type, int16_t* buf, 
 			/* alternative 2Pole LP filter to fix the kick transient problems with the nonlinear ZDF LP */
 
 			float x = softClipTwo((buf[i]/((float)0x7fff))*filter->drive);
-			float q = (1-filter->q) *2.5 ;/// (1.0 - filter->f);
+			//float q = (1-filter->q) *2.5 ;/// (1.0 - filter->f);
+			float q = (1-filter->q) *1.5 + (1-filter->q) / (1.0 - filter->f);
 
 			filter->a += filter->f * ((x - filter->a)  + q * (filter->a - filter->b ));
 			if(filter->a > 1) filter->a = 1;
